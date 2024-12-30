12f9632efe75e8a85b2efee6a9a5fe1b841b877e
Khane <Sanika.Khane@Klingelnberg.com>
2024-12-30 12:39:48 +0530

    learnt Using LINQ Select and Order Operations

---
+++ DataManager.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace Hands_on
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        public Employee()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Age = 0;
+            Department = string.Empty;
+            Salary = 0;
+        }
+        public Employee(int id,string firstName,string lastName,int age,string department,int salary)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Department = department;
+            Salary = salary;
+        }
+    }
+    public static class Datamanager
+    {
+        public static List<Employee> GetData()
+        {
+            List<Employee> employees = new List<Employee>
+                {
+                   new Employee(1, "Sanika", "Khane", 21, "Intern", 12000),
+                   new Employee(2, "Amit", "Sharma", 29, "HR", 40000),
+                   new Employee(3, "Rohit", "Verma", 35, "IT", 60000),
+                   new Employee(4, "Pooja", "Singh", 25, "Finance", 35000),
+                   new Employee(5, "Neha", "Patil", 30, "Marketing", 45000),
+                   new Employee(6, "Suresh", "Mishra", 40, "Admin", 50000),
+                   new Employee(7, "Ankita", "Roy", 26, "IT", 55000),
+                   new Employee(8, "Dev", "Desai", 38, "Sales", 47000),
+                   new Employee(9, "Sanika", "Kulkarni", 24, "Intern", 15000),
+                   new Employee(10, "Arjun", "Kumar", 32, "Operations", 52000),
+                   new Employee(11, "Meera", "Gupta", 27, "Finance", 37000),
+                   new Employee(12, "Raj", "Kapoor", 41, "IT", 62000),
+                   new Employee(13, "Manisha", "Dutta", 28, "HR", 42000),
+                   new Employee(14, "Prakash", "Yadav", 36, "Admin", 51000),
+                   new Employee(15, "Sanika", "Bose", 23, "Marketing", 34000),
+                   new Employee(16, "Anil", "Pawar", 33, "Sales", 49000),
+                   new Employee(17, "Kavita", "Mehta", 22, "Intern", 14000),
+                   new Employee(18, "Dev", "Joshi", 37, "Operations", 53000),
+                   new Employee(19, "Nikita", "Singh", 25, "Finance", 36000),
+                   new Employee(20, "Ravi", "Chopra", 39, "Admin", 52000)
+                 };
+                return employees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Hands_on
 {
@@ -6,30 +8,58 @@ namespace Hands_on
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(isEqual("abc", "xyz"));
-            Console.WriteLine(isEqual(2,2));
+            List<Employee> employees= Datamanager.GetData();
 
-            CalculateSum<int> calculateSum = new CalculateSum<int>();
-            Console.WriteLine(calculateSum.add(5, 3));
-            
+            //It selects only the firtname of the employees
+            /*IEnumerable<string> names = employees.Select(emp => emp.FirstName);
+            foreach (string name in names) { Console.WriteLine(name); }*/
 
-            CalculateSum<double> calculate = new CalculateSum<double>();
-            Console.WriteLine(calculate.add(5.6, 9.3));
-        }
-        //Generic method
-        public static bool isEqual<T>(T a, T b)
-        {
-            return a.Equals(b);
-        }
-    }
-    //Generic class
-    public class CalculateSum<T> where T: struct
-    {
-        public T add(T a, T b)
-        {
-            dynamic x = a;
-            dynamic y = b;
-            return x + y;
+            //If we want the unqui values distict is used
+            /*IEnumerable<string> names = employees.Select(emp => emp.FirstName).Distinct();
+            int count= employees.Count();
+            Console.WriteLine(count);
+            Console.WriteLine(names.Count());
+            foreach (string name in names) { Console.WriteLine(name); }*/
+
+            //Order By name only 
+            //employees=employees.OrderBy(x => x.FirstName).ToList();
+
+            //If want to add another parameter for ordering then "ThenBy" is used
+            //employees = employees.OrderByDescending(e => e.FirstName).ThenBy(e=>e.LastName).ToList();
+
+
+            //For adding the condition where clause is used
+            //employees = employees.Where(e => e.Age > 30).ToList();
+
+            //if we want a single value or record from the collection the 
+            //Employee emp = employees.First(e=> e.FirstName=="Sanika");
+
+            //If the element is not present then it will throw error so FirstOrdefault is used it will return null
+            //Employee emp = employees.FirstOrDefault(e => e.FirstName == "Saanika");
+
+            //Employee emp = employees.Last(e => e.FirstName == "Sanika");
+            //Employee emp1 = employees.LastOrDefault(e => e.FirstName == "Sanika");
+
+            //if there is only one unique recodr then signle is used
+            //Employee emp = employees.Single(emp => emp.FirstName == "Meera");
+
+
+
+            //It will only consider the records of how much count is given from start
+            //employees = employees.Take(3).ToList();
+
+            employees = employees.DistinctBy(x => x.FirstName).ToList();
+
+
+            //Console.WriteLine($"Id : {emp.Id} FirstName : {emp.FirstName} LastName : {emp.LastName} Age : {emp.Age}" +
+            //                     $" Department : {emp.Department} Salary : {emp.Salary}");
+
+            employees.ForEach(emp => 
+                       Console.WriteLine($"Id : {emp.Id} FirstName : {emp.FirstName} LastName : {emp.LastName} Age : {emp.Age} Department : {emp.Department} Salary : {emp.Salary}"));
+
+
+
+
         }
     }
 }
