22179f2359dd5bd5683638f4d49676c888fc30ac
Khane <Sanika.Khane@Klingelnberg.com>
2024-12-30 16:34:10 +0530

    Learnt normal join and group Join

--- Program.cs
+++ Program.cs
@@ -1,65 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace Hands_on
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            List<Employee> employees= Datamanager.GetData();
+            List<Person> persons = new List<Person>
+            {
+                new Person { Id = 1, Name = "John" },
+                new Person { Id = 2, Name = "Emily" },
+                new Person { Id = 3, Name = "Michael" },
+                new Person { Id = 4, Name = "Sophia" }
+            };
+            List<Order> orders = new List<Order>
+            {
+                new Order { OrderId = 101, PersonId = 1, Product = "Laptop" },
+                new Order { OrderId = 102, PersonId = 1, Product = "Mouse" },
+                new Order { OrderId = 103, PersonId = 2, Product = "Keyboard" },
+                new Order { OrderId = 104, PersonId = 3, Product = "Monitor" }
+            };
+            //Normal Join
+            /*var normalJoin = persons.Join(
+                orders,
+                person => person.Id,        
+                order => order.PersonId,    
+                (person, order) => new     
+                {
+                    PersonName = person.Name,
+                    Product = order.Product
+                });
 
-            //It selects only the firtname of the employees
-            /*IEnumerable<string> names = employees.Select(emp => emp.FirstName);
-            foreach (string name in names) { Console.WriteLine(name); }*/
+            foreach (var item in normalJoin)
+            {
+                Console.WriteLine($"Person: {item.PersonName}, Product: {item.Product}");
+            }*/
 
-            //If we want the unqui values distict is used
-            /*IEnumerable<string> names = employees.Select(emp => emp.FirstName).Distinct();
-            int count= employees.Count();
-            Console.WriteLine(count);
-            Console.WriteLine(names.Count());
-            foreach (string name in names) { Console.WriteLine(name); }*/
+            //Group join
+            var groupJoin = persons.GroupJoin(
+                orders,
+                person => person.Id,        
+                order => order.PersonId,    
+                (person, ordersGroup) => new 
+                {
+                    PersonName = person.Name,
+                    Orders = ordersGroup     
+                });
 
-            //Order By name only 
-            //employees=employees.OrderBy(x => x.FirstName).ToList();
-
-            //If want to add another parameter for ordering then "ThenBy" is used
-            //employees = employees.OrderByDescending(e => e.FirstName).ThenBy(e=>e.LastName).ToList();
-
-
-            //For adding the condition where clause is used
-            //employees = employees.Where(e => e.Age > 30).ToList();
-
-            //if we want a single value or record from the collection the 
-            //Employee emp = employees.First(e=> e.FirstName=="Sanika");
-
-            //If the element is not present then it will throw error so FirstOrdefault is used it will return null
-            //Employee emp = employees.FirstOrDefault(e => e.FirstName == "Saanika");
-
-            //Employee emp = employees.Last(e => e.FirstName == "Sanika");
-            //Employee emp1 = employees.LastOrDefault(e => e.FirstName == "Sanika");
-
-            //if there is only one unique recodr then signle is used
-            //Employee emp = employees.Single(emp => emp.FirstName == "Meera");
-
-
-
-            //It will only consider the records of how much count is given from start
-            //employees = employees.Take(3).ToList();
-
-            employees = employees.DistinctBy(x => x.FirstName).ToList();
-
-
-            //Console.WriteLine($"Id : {emp.Id} FirstName : {emp.FirstName} LastName : {emp.LastName} Age : {emp.Age}" +
-            //                     $" Department : {emp.Department} Salary : {emp.Salary}");
-
-            employees.ForEach(emp => 
-                       Console.WriteLine($"Id : {emp.Id} FirstName : {emp.FirstName} LastName : {emp.LastName} Age : {emp.Age} Department : {emp.Department} Salary : {emp.Salary}"));
-
-
-
-
+            foreach (var item in groupJoin)
+            {
+                Console.WriteLine($"Person: {item.PersonName}");
+                foreach (var order in item.Orders)
+                {
+                    Console.WriteLine($"  Product: {order.Product}");
+                }
+            }
         }
     }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int PersonId { get; set; }
+        public string Product { get; set; }
+    }
+
 }
